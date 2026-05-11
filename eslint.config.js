ce4d33ee2c413deec58cb4145ee7f4df5003a314
Omar Villamizar <omarjesusvi@ufps.edu.co>
2026-05-11 07:13:17 -0500

    chore: setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "eqeqeq": ["error", "always"],
+      "no-unused-vars": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
